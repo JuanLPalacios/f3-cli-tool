80d0c04c0bf8eef274475acdb720d9b84477695b
JuanLP <juan.luis.palacios.p@gmail.com>
2022-07-30 23:26:10 -0500

    Add dsn format for mysql and sqlite

--- index.php
+++ index.php
@@ -21,6 +21,7 @@ function generateAs($template, $file, $overwrite = null) {
   $message = file_exists($file) ? COLOR->ok('overwrite') : COLOR->ok('created');
   $fs = fopen($file, "w") or die("Unable to write to {$file}!");
   echo "[{$message}]{$file}\n";
+  //echo sprintf("%s\n",realpath($file));
   fwrite($fs, \Template::instance()->render($template));
   fclose($fs);
 }
@@ -50,7 +51,7 @@ $app
     ->option('-y, --yes-default', 'yes to defaults')
     ->action(function ($name, $description, $license, $yesDefault) {
       $interactor = new Ahc\Cli\IO\Interactor;
-      $dirPath =  $name ?: '';
+      $dirPath =  $name ?: '.';
       $name = $name ?: basename(realpath($dirPath));
       if(!$yesDefault) $name = $interactor->prompt("Project name", $name);
       if(!$yesDefault) $description = $interactor->prompt('description', $description);
@@ -78,18 +79,65 @@ $app
   ->add((new Ahc\Cli\Input\Command('datasource', 'Add datasource'))
     ->action(function ($name, $description, $license, $yesDefault) {
       $interactor = new Ahc\Cli\IO\Interactor;
-      $dirPath = '';
       $name = $interactor->prompt("Data source name");
       $client = $interactor->choice('Select the client', ['Jig', 'Mongo', 'SQL'], 'Jig');
-      
-      
+      switch ($client) {
+        case 'Jig':
+          $params = [
+            "dir" => $interactor->prompt("Storage directory", "../data/{$name}/"),
+            "format" => $interactor->choice('Format', ["JSON" => \DB\Jig::FORMAT_JSON , "Serialized" => \DB\Jig::FORMAT_Serialized], 'JSON')
+          ];
+          break;
+        
+        case 'Mongo':
+          $params = [
+            "dsn" => $interactor->prompt("Server", 'mongodb://localhost:27017'),
+            "dbname" => $interactor->prompt("Database name"),
+            "options" => []
+          ];
+          break;
+          
+        default:
+          $engine = $interactor->choice('Engine', [
+            "mysql" => 'MySQL 5.x',
+            "sqlite" => 'SQLite 3 and SQLite 2',
+            "pgsql" => 'PostgreSQL',
+            "sqlsrv" => 'Microsoft SQL Server / SQL Azure',
+            "mssql" => 'dblib, sybase: FreeTDS / Microsoft SQL Server / Sybase',
+            "odbc" => 'ODBC v3',
+            "oci" => 'Oracle'
+          ], 'mysql');
+          switch ($engine) {
+            case 'mysql':
+              $host = $interactor->prompt('Host', 'localhost');
+              $port = $interactor->prompt('Port', 3307);
+              $dbname = $interactor->prompt("DB Name", $name);
+              $dsn = "mysql:host={$host};port={$port};dbname={$dbname}";
+              break;
+            case 'sqlite':
+              $file = $interactor->prompt('File', ':memory:');
+              $dsn = "sqlite:{$file}";
+              break;
+            default:
+              break;
+          }
+          $params = [
+            "dsn" => "{$engine}:",
+            "user" => "",
+            "pw" => "",
+            "options" => []
+          ];
+          break;
+      }
       $f3=Base::instance();
-      $f3->set('sources.' . $name, ["client" => $client]);
+      $f3->config('./config/config.dev.ini');
+      
+      $f3->set('sources.' . $name, array_merge(["client" => $client], $params));
       generateFiles([
         'config\config.dev.ini',
         'config\config.production.ini',
         'config\config.test.ini'
-      ], $dirPath, $yesDefault);
+      ], '.', true);
       deleteTmp();
       }));
       /*
