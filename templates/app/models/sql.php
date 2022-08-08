5a93f3048cc54324007bd3eb28f11a5d63a7a245
JuanLP <juan.luis.palacios.p@gmail.com>
2022-08-08 18:29:56 -0500

    Add datasource instance permanence on sql model

--- templates/app/models/sql.php
+++ templates/app/models/sql.php
@@ -2,7 +2,9 @@
 namespace Model;
 
 class {{@name | capitalize}} extends \DB\SQL\Mapper {
-	function __construct() {
-		parent::__construct(new DB\SQL({{'{{'}}@sources['{{@source_name}}']['dsn']{{'}}'}}, {{'{{'}}@sources['{{@source_name}}']['user']{{'}}'}}, {{'{{'}}@sources['{{@source_name}}']['pw']{{'}}'}}), '{{@name}}' );
+	function __construct($f3=\Base::instance()) {
+		parent::__construct($f3->exists('sources.{{@source_name}}.instance', $source)?$source:new \DB\SQL($f3-get('sources.{{@source_name}}.dsn'), $f3-get('sources.{{@source_name}}.user'), $f3-get('sources.{{@source_name}}.pw')), '{{@table_name}}' );
+		if(!isset($source))
+			$f3->set($this->db);
 	}
 }
