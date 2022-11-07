4cfe9fe18bd9740ab5cec171c3b32bcf16c4cca3
JuanLP <juan.luis.palacios.p@gmail.com>
2022-11-07 07:46:28 -0500

    Fix refatoring errors

--- bin/f3-cli-tool.php
+++ bin/f3-cli-tool.php
@@ -19,5 +19,8 @@ $f3->set('AUTOLOAD', __DIR__  . '/../src/' );
 $app = new Ahc\Cli\Application('f3-cli-tool', '0.1.0');
 
 $app->add(new Commands\App, 'a');
+$app->add(new Commands\DataSource, 'ds');
+$app->add(new Commands\Model, 'm');
+$app->add(new Commands\Scaffolding, 's');
       
 $app->logo($logo)->handle($_SERVER['argv']);
